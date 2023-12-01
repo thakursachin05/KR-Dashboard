2826d0fceee377813cdafe1738e151c7c864c80c
Ashutosh741 <106524453+Ashutosh741@users.noreply.github.com>
2023-12-01 15:13:48 +0530

    only login page render

--- src/App.js
+++ src/App.js
@@ -32,15 +32,15 @@ function App() {
     <>
       <Router>
         <Routes>
-          <Route path="/login" element={<Login />} />
-          <Route path="/forgot-password" element={<ForgotPassword />} />
-          <Route path="/register" element={<Register />} />
+          <Route path="/" element={<Login />} />
+          {/* <Route path="/forgot-password" element={<ForgotPassword />} /> */}
+          {/* <Route path="/register" element={<Register />} /> */}
           {/* <Route path="/" element={<Login />} /> */}
           
           {/* Place new routes over this */}
-          <Route path="/app/*" element={<Layout />} />
+          {/* <Route path="/app/*" element={<Layout />} /> */}
 
-          <Route path="*" element={<Navigate to={token ? "/dashboard" : "/login"} replace />}/>
+          {/* <Route path="*" element={<Navigate to={token ? "/dashboard" : "/login"} replace />}/> */}
 
         </Routes>
       </Router>
