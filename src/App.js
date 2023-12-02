8433601fc5d7d54fdc85fb121827e538c2a8fa99
Ashutosh741 <106524453+Ashutosh741@users.noreply.github.com>
2023-12-02 19:07:07 +0530

    tseting of login route

--- src/App.js
+++ src/App.js
@@ -38,8 +38,8 @@ function App() {
           <Route path="/forgot-password" element={<ForgotPassword />} />
           <Route path="/register" element={<Register />} />
           <Route path="/documentation" element={<Documentation />} />
+          <Route path="/" element={<Login />} />
           
-          {/* Place new routes over this */}
           <Route path="/app/*" element={<Layout />} />
 
           <Route path="*" element={<Navigate to={token ? "/app/welcome" : "/login"} replace />}/>
