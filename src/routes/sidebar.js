25f02cb18e8f60d7ef7090a84979ed038b0dcead
Ashutosh741 <106524453+Ashutosh741@users.noreply.github.com>
2023-12-05 03:29:24 +0530

    remove unused code and added admin key

--- src/routes/sidebar.js
+++ src/routes/sidebar.js
@@ -1,48 +1,43 @@
+// import Squares2X2Icon from "@heroicons/react/24/outline/Squares2X2Icon";
+// import WalletIcon from "@heroicons/react/24/outline/WalletIcon";
+import UserIcon from "@heroicons/react/24/outline/UserIcon";
+// import CurrencyDollarIcon from "@heroicons/react/24/outline/CurrencyDollarIcon";
+import InboxArrowDownIcon from "@heroicons/react/24/outline/InboxArrowDownIcon";
+import UsersIcon from "@heroicons/react/24/outline/UsersIcon";
 
-import Squares2X2Icon from '@heroicons/react/24/outline/Squares2X2Icon'
-import WalletIcon from '@heroicons/react/24/outline/WalletIcon'
-import UserIcon from '@heroicons/react/24/outline/UserIcon'
-import CurrencyDollarIcon from '@heroicons/react/24/outline/CurrencyDollarIcon'
-import InboxArrowDownIcon from '@heroicons/react/24/outline/InboxArrowDownIcon'
-import UsersIcon from '@heroicons/react/24/outline/UsersIcon'
-
-const iconClasses = `h-6 w-6`
-const submenuIconClasses = `h-5 w-5`
+const iconClasses = `h-6 w-6`;
+const submenuIconClasses = `h-5 w-5`;
+const isAdmin = localStorage.getItem('isAdmin') === 'true';
 
 const routes = [
+  {
+    path: "/app/leads",
+    icon: <InboxArrowDownIcon className={iconClasses} />, 
+    name: "Leads", 
+  },
+  {
+    path: "/app/settings-profile",
+    icon: <UserIcon className={submenuIconClasses} />, 
+    name: "Profile", 
+  },
+];
 
-  {
-    path: '/app/dashboard',
-    icon: <Squares2X2Icon className={iconClasses}/>, 
-    name: 'Dashboard',
-  },
-  {
-    path: '/app/leads', // url
-    icon: <InboxArrowDownIcon className={iconClasses}/>, // icon component
-    name: 'Leads', // name that appear in Sidebar
-  },
-  {
-    path: '/app/transactions', // url
-    icon: <CurrencyDollarIcon className={iconClasses}/>, // icon component
-    name: 'Transactions', // name that appear in Sidebar
-  },
-  {
-    path: '/app/settings-profile', //url
-    icon: <UserIcon className={submenuIconClasses}/>, // icon component
-    name: 'Profile', // name that appear in Sidebar
-  },
-  {
-    path: '/app/settings-billing',
-    icon: <WalletIcon className={submenuIconClasses}/>,
-    name: 'Billing',
-  },
-  {
-    path: '/app/settings-team', // url
-    icon: <UsersIcon className={submenuIconClasses}/>, // icon component
-    name: 'Team Members', // name that appear in Sidebar
-  },
-]
+// Only add the following routes if isAdmin is true
+if (isAdmin) {
+  routes.push(
+    {
+      path: "/app/teamMembers",
+      icon: <UsersIcon className={submenuIconClasses} />, 
+      name: "Team Members", 
+    },
+    {
+      path: "/app/addMember",
+      icon: <UserIcon className={submenuIconClasses} />, 
+      name: "Add New Member", 
+    },
+  );
+}
 
-export default routes
+export default routes;
 
 
