114e1580697a24de9273d36371ddea2a1887e98c
Ashutosh741 <106524453+Ashutosh741@users.noreply.github.com>
2023-12-06 21:14:48 +0530

    conflict resolved, error removed

--- src/features/user/LandingIntro.js
+++ src/features/user/LandingIntro.js
@@ -1,5 +1,5 @@
 import TemplatePointers from "./components/TemplatePointers"
-import logo from '../../assets/images/logo.png'
+// import logo from '../../assets/images/logo.png'
 
 
 function LandingIntro(){
