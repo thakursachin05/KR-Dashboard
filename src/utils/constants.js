5c511c9daf9e68e396583e850e6eca0461482fb4
Ashutosh741 <106524453+Ashutosh741@users.noreply.github.com>
2024-03-06 12:27:03 +0530

    changed route

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,5 +1,5 @@
-// export const API = "https://krteleservices-backend.onrender.com/api";
+export const API = "https://krteleservices-backend.onrender.com/eft/api";
 
-export const API = "https://api.thekrteleservices.com/eft/api";
+// export const API = "https://api.thekrteleservices.com/eft/api";
 
 // export const API = "http://3.111.103.148:8800/api";
